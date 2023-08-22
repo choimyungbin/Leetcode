ee046153f15a0c842546a9716d36a415437b8434
최명빈 <71762087+choimyungbin@users.noreply.github.com>
2023-08-21 19:34:40 -0500

    Time: 133 ms (84.94%), Space: 24.8 MB (9.05%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # make hashMap
+        hashMap = collections.defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                hashMap[pattern].append(word)
+        
+        # BFS
+        res = 1
+        q = collections.deque([beginWord])
+        visit = set([beginWord])
+        while q:
+            for _ in range(len(q)):
+                word = q.popleft()
+                # base case
+                if word == endWord:
+                    return res
+                
+                for i in range(len(word)):
+                    pattern = word[:i] + "*" + word[i+1:]
+                    for nei in hashMap[pattern]:
+                        if nei not in visit:
+                            visit.add(nei)
+                            q.append(nei)
+            res += 1
+                
+                
+        return 0
+                
