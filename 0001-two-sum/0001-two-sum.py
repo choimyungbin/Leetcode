9287402a97a186ae01aa5d1948fb545d3708269f
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-09-24 11:56:40 -0500

    Time: 69 ms (54.21%), Space: 17.6 MB (30.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i, n in enumerate(nums):
+            if target-n in hashMap:
+                return [i, hashMap[target-n]]
+            
+            hashMap[n] = i
