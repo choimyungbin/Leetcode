7e1e3d758eb4bd59ee68cbb32938827c5e3b85f1
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-10-06 02:34:21 -0500

    Time: 103 ms (90.18%), Space: 17.8 MB (49.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        k = len(nums)/3
+        k = len(nums)//3
         hashMap = {}
         res = []
+        # hashMap == Counter(nums)
         for n in nums:
             if n in hashMap:
                 hashMap[n] += 1
             else:
                 hashMap[n] = 1
         
-        for key in hashMap.keys():
-            if hashMap[key] > k:
+        for key, item in hashMap.items():
+            if item > k:
                 res.append(key)
         return res
+    
