ed228b1cac8f1f6df9f3a616d354c27e05491210
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-10-16 15:39:23 -0500

    Time: 36 ms (74.45%), Space: 16.1 MB (96.59%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,13 +1,14 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        def fact(n):
-            if n <= 1:
-                return 1
-            else:
-                return n*fact(n-1)
-        
-        res = []
-        for i in range(rowIndex+1):
-            res.append(fact(rowIndex)//(fact(rowIndex-i)*fact(i)))
-            
+        res = [1]
+        prev = 1
+        for k in range(1, rowIndex + 1):
+            next_val = prev * (rowIndex - k + 1) // k
+            res.append(next_val)
+            prev = next_val
         return res
+    # 4C0 4C1 4C2 4C3 4C4
+    # 1    4    6  4   1
+    # 4!/0!4!
+    # 4!/1!3!
+    # 4!/2!2!
