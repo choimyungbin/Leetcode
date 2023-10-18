8b90520ed6de62e9434b7711ef80f66b0f1314da
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-10-18 00:48:53 -0500

    Time: 260 ms (85.41%), Space: 19.9 MB (38.91%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,28 +1,31 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        # root, one parent, all connected, no cycle
+        
         def find_root():
-            # dfs
+            # set with OR: automatic sorting & remove duplicates
             children = set(leftChild) | set(rightChild)
             for i in range(n):
                 if i not in children:
                     return i
-
             return -1
         
+        # 1. find root idx
         root = find_root()
         if root == -1:
             return False
         
-        seen = {root}
+        # 2. dfs using stack
+        seen = {root}   # set
         stack = [root]
         while stack:
             node = stack.pop()
             for child in [leftChild[node], rightChild[node]]:
                 if child != -1:
+                    # see multiple parents (or cycle, which has multiple parents)
                     if child in seen:
                         return False
                     
                     stack.append(child)
                     seen.add(child)
-                    
         return len(seen) == n
