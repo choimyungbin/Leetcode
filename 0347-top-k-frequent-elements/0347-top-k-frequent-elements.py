885d85ef7fe0b52d832d4280548aa17bda3ef81f
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-10-18 15:05:39 -0500

    Time: 109 ms (18.31%), Space: 21.8 MB (26.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        # use freq as idx
-        buck = [[] for i in range(len(nums)+1)]
-        
+        # bc the biggest num nums can haave is len(nums)
+        buck = [[] for _ in range(len(nums) + 1)]
+        res = []
+
         for n in nums:
-            count[n] = 1+count.get(n,0)
-        for n,c in count.items():
+            count[n] = 1+count.get(n, 0)
+        # {1:3, 2:2, 3:1}
+        for n, c in count.items():
             buck[c].append(n)
         # [[], [3], [2], [1], [], []]
-        res = []
-        # iterate backward to get most frequent numbers first
-        for i in range(len(buck)-1,0,-1):
+        for i in range(len(buck)-1, 0, -1):
             for e in buck[i]:
                 res.append(e)
-                if len(res)==k:
+                if len(res) == k:
                     return res
