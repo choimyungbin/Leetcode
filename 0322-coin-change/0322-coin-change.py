cc9a13365c677f3291a595541d123e19b09cd709
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2023-12-29 17:48:07 -0600

    Time: 2077 ms (5.01%), Space: 17.6 MB (37.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse=True)
-        dp = {0:0}
-        for c in coins:
-            dp[c] = 1
-        def dfs(amount):
-            li = []
-            if amount in dp:
-                return dp[amount]
-            if amount < coins[-1]:
-                return -1
+        # dp
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+        for i in range(1, amount+1):
             for c in coins:
-                if (amount-c)>0:
-                    subset = dfs(amount-c)
-                    dp[amount-c] = subset
-                    if subset == -1:
-                        continue
-                    li.append(1+subset)
-            if not li:
-                return -1
-            return min(li)
-        
-        return dfs(amount)
+                if i-c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
+        return dp[amount] if dp[amount] < amount+1 else -1
