6dcdd2f05d90cf76505ba9d3fa610188abf88e96
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-01-08 09:12:05 -0600

    Time: 714 ms (85.60%), Space: 21.6 MB (21.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        hashMap = {}
-        nums.sort()
-        
-        for i, n in enumerate(nums):
-            # important!!!
-            # if n is not first num but the biggest
-            if i>0 and n == nums[i-1]:
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            # important!!
-            # set l = i+1
+            target = 0 - nums[i]
             l, r = i+1, len(nums)-1
             while l < r:
-                total = n+nums[l]+nums[r]
-                if total < 0:
+                if nums[l] + nums[r] == target:
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
-                elif total > 0:
+                    r -= 1
+                    # ⭐️⭐️⭐️
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                elif nums[l] + nums[r] > target:
                     r -= 1
                 else:
-                    res.append([n, nums[l], nums[r]])
                     l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
+        
         return res
-                    
