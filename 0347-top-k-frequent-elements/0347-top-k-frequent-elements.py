29e2517e475d6fc9b4087fd0d62d23affcf10790
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-01-10 17:46:02 -0600

    Time: 94 ms (70.24%), Space: 22.8 MB (11.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        # bc the biggest num nums can haave is len(nums)
-        buck = [[] for _ in range(len(nums) + 1)]
+        counter = Counter(nums)
+        countList = [[] for _ in range(len(nums)+1)]
+        order = 0
         res = []
-
-        for n in nums:
-            count[n] = 1+count.get(n, 0)
-        # {1:3, 2:2, 3:1}
-        for n, c in count.items():
-            buck[c].append(n)
-        # [[], [3], [2], [1], [], []]
-        for i in range(len(buck)-1, 0, -1):
-            for e in buck[i]:
-                res.append(e)
-                if len(res) == k:
-                    return res
+        
+        for n in counter.keys():
+            countList[counter[n]].append(n)
+        for i in range(len(nums),-1,-1):
+            for n in countList[i]:
+                if order < k:
+                    res.append(n)
+                    order += 1
+        
+        return res
+                
+        
