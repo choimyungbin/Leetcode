d76114cd29979ac762c11339b7469f3044105807
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-01-11 10:41:29 -0600

    Time: 196 ms (85.35%), Space: 18.8 MB (18.73%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,6 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        hashSet = set(nums)
-        if target not in hashSet:
-            return -1
-        return bisect_left(nums, target)
+        l, r = 0, len(nums)-1
+        res = -1
+        while l < r:
+            if target > nums[(l+r)//2]:
+                l = (l+r)//2 + 1
+            elif target < nums[(l+r)//2]:
+                r = (l+r)//2 - 1
+            else:
+                res = (l+r)//2
+                break
+        if l == r and nums[l] == target:
+            res = l
+        return res
