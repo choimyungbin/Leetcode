eed8049de64a0cbaa9f384ab47242538b8193e37
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-01-22 11:02:43 -0600

    Time: 163 ms (72.50%), Space: 18.5 MB (57.50%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,11 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        # 1. get duplicate
         numSet = set()
         duplicate = 0
         missing = 0
         for n in nums:
-            # not duplicate
             if n not in numSet:
                 numSet.add(n)
-            # duplicate
             else:
                 duplicate = n
         for n in range(1,len(nums)+1):
