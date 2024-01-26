e4803346c4aad7432e04dea9c9432a928d3f91d4
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-01-26 12:09:07 -0600

    Time: 77 ms (85.26%), Space: 22.8 MB (28.90%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        # should practice more
+        @lru_cache(None)
+        def recursive_run(i,j,moves):
+            if i>=m or j>=n or i<0 or j<0:
+                return 1
+            elif moves == 0:
+                return 0
+            out = recursive_run(i+1,j,moves-1)
+            out += recursive_run(i-1,j,moves-1)
+            out += recursive_run(i,j+1,moves-1)
+            out += recursive_run(i,j-1,moves-1)
+            return out
+        return recursive_run(startRow,startColumn,maxMove) %(10**9+7)
