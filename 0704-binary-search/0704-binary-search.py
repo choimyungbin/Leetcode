11f75dea7e210fd32f0e86670111182997990c86
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-02-27 02:25:11 -0600

    Time: 196 ms (42.43%), Space: 18 MB (97.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,13 +3,11 @@ class Solution:
         l, r = 0, len(nums)-1
         res = -1
         while l < r:
-            if target > nums[(l+r)//2]:
-                l = (l+r)//2 + 1
-            elif target < nums[(l+r)//2]:
-                r = (l+r)//2 - 1
+            m = (l+r)//2
+            if target > nums[m]:
+                l = m+1
             else:
-                res = (l+r)//2
-                break
-        if l == r and nums[l] == target:
-            res = l
+                r = m
+        if nums[l] == target:
+            res = l     
         return res
