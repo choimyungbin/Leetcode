c008883d9aabb639e06d6cc5f3eb4e73fa0bc4c4
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-03-31 20:11:52 -0500

    Time: 42 ms (10.43%), Space: 16.8 MB (6.34%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        p = len(s)-1
+        count = 0
+        while s[p] == ' ':
+            p -= 1
+        while p>=0 and s[p] != ' ':
+            count += 1
+            p -= 1
+            
+        return count
