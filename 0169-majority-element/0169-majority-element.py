3053ab80ea676c4b602ff6a572bc961c952ed8f4
Myoungbin (Jessica) Choi <71762087+choimyungbin@users.noreply.github.com>
2024-11-20 23:24:46 -0600

    Time: 11 ms (32.70%), Space: 18.5 MB (12.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hashMap = {}
-        majority = len(nums) // 2
+        threshold = len(nums)//2
         for n in nums:
-            hashMap[n] = 1+hashMap.get(n, 0)
-            if hashMap[n] > majority:
+            hashMap[n] = hashMap.get(n, 0) + 1
+            if hashMap[n] > threshold:
                 return n
